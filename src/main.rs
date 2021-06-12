ca35994e5bd088a22cd7014a38a494e467883925
yuniruyuni <yuniruyuni@gmail.com>
2021-06-12 21:27:32 +0900

    add use for map/set template code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use proconio::input;
 // use proconio::marker::Chars;
 // use proconio::marker::Bytes;
+// use std::collections::HashMap;
+// use std::collections::BTreeMap;
+// use std::collections::HashSet;
+// use std::collections::BTreeSet;
 
 fn main() {
     input! {
     }
 
-    println!("");
+    println!("{}");
 }

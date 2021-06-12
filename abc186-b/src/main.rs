277512da8161feebe88d880c35e36f89aad4cbea
yuniruyuni <yuniruyuni@gmail.com>
2021-06-13 02:01:03 +0900

    optimize abc186-b

--- abc186-b/src/main.rs
+++ abc186-b/src/main.rs
@@ -8,30 +8,14 @@ fn main() {
         cells: [usize; h*w],
     }
 
-    let mut hist: [i32; 101] = [0; 101];
-
+    let mut sum = 0;
     let mut min = std::usize::MAX;
-    let mut max = std::usize::MIN;
     for &cell in &cells {
         min = min.min(cell);
-        max = max.max(cell);
-        hist[cell] += 1;
+        sum += cell;
     }
 
-    let mut ans = std::i32::MAX;
-    for j in 0..=min {
-        if hist[j] == 0 { continue }
-
-        let mut pruned = 0;
-
-        for i in min..=max {
-            let target = if i < j { 0 } else { j };
-            let diff = (i - target) as i32;
-            pruned += hist[i] * diff;
-        }
-
-        ans = ans.min(pruned);
-    }
-
-    println!("{}", ans);
+    // all cells needs to go to min height
+    // so we can calculate this value by following formula.
+    println!("{}", sum - min*h*w);
 }

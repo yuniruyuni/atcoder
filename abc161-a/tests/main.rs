bfec2fa438724fcd19671a1052f4a336c867fac8
yuniruyuni <yuniruyuni@gmail.com>
2021-06-12 21:33:55 +0900

    add workspace for abc161-a

---
+++ abc161-a/tests/main.rs
@@ -0,0 +1,25 @@
+use cli_test_dir::*;
+use unindent::unindent;
+
+fn assert_cmd(stdin: &str, stdout: &str) {
+    let testdir = TestDir::new("./abc161-a", "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(unindent(stdin))
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), unindent(stdout));
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample1() {
+    assert_cmd(
+        r#"
+            1 2 3
+        "#,
+        r#"
+            3 1 2
+        "#
+    );
+}

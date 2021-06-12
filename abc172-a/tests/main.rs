a4df4f1a477bdb1b5fb4b2178415d9c5c2daca46
yuniruyuni <yuniruyuni@gmail.com>
2021-06-12 21:39:39 +0900

    add workspace for abc172-a

---
+++ abc172-a/tests/main.rs
@@ -0,0 +1,37 @@
+use cli_test_dir::*;
+use unindent::unindent;
+
+fn assert_cmd(stdin: &str, stdout: &str) {
+    let testdir = TestDir::new("./abc172-a", "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(unindent(stdin))
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), unindent(stdout));
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample1() {
+    assert_cmd(
+        r#"
+            2
+        "#,
+        r#"
+            14
+        "#
+    );
+}
+
+#[test]
+fn sample2() {
+    assert_cmd(
+        r#"
+            10
+        "#,
+        r#"
+            1110
+        "#
+    );
+}

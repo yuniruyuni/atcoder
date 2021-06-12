8d4dde98a808a2263f938c3f78a4fe5e17dadff9
yuniruyuni <yuniruyuni@gmail.com>
2021-06-12 21:05:13 +0900

    solve abc176-a

---
+++ abc176-a.rs
@@ -0,0 +1,14 @@
+// proconio = "*"
+
+use proconio::input;
+
+fn main() {
+    input! {
+        n: i32, x: i32, t: i32,
+    }
+    println!("{}", ((n + (x-1)) / x) * t);
+    // another solution:
+    // (n + x - 1) / x = ((n - 1) / x) + (x / x) = (n-1)/x + 1
+    // so...
+    // println!("{}", ((n-1)/x + 1)*t);
+}

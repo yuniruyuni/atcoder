994ff28fd079c653a0988bb41a9c8d21026d7345
yuniruyuni <yuniruyuni@gmail.com>
2021-06-12 23:31:03 +0900

    refine solution for abc170-a

--- abc170-a/src/main.rs
+++ abc170-a/src/main.rs
@@ -2,14 +2,10 @@ use proconio::input;
 
 fn main() {
     input! {
-        x1: i32,
-        x2: i32,
-        x3: i32,
-        x4: i32,
-        x5: i32,
+        x: (i32, i32, i32, i32, i32),
     }
 
-    let i = match (x1, x2, x3, x4, x5) {
+    let i = match x {
         (0, _, _, _, _) => 1,
         (_, 0, _, _, _) => 2,
         (_, _, 0, _, _) => 3,
